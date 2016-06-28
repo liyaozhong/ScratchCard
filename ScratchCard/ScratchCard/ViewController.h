5f101b44dd8684e0fb92338a6da57bb631f0d285
liyaozhong <yun.zhongyue@163.com>
2016-06-28 15:16:49 +0800

    setup project

---
+++ ScratchCard/ScratchCard/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ScratchCard
+//
+//  Created by joshuali on 16/6/28.
+//  Copyright © 2016年 joshuali. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ScratchCard/ScratchCard/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ScratchCard
+//
+//  Created by joshuali on 16/6/28.
+//  Copyright © 2016年 joshuali. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
