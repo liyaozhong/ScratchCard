16c54c87a57d481c0dc1cb35e17be678b88d3f97
liyaozhong <yun.zhongyue@163.com>
2016-06-28 15:19:46 +0800

    add scratch view

---
+++ ScratchCard/ScratchCard/ScratchView.h
@@ -0,0 +1,14 @@
+//
+//  ScratchView.h
+//  ScratchCard
+//
+//  Created by joshuali on 16/6/28.
+//  Copyright © 2016年 joshuali. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+IB_DESIGNABLE
+@interface ScratchView : UIView
+@property (nonatomic) IBInspectable CGFloat scratchLineWidth;
+@end
---
+++ ScratchCard/ScratchCard/ScratchView.m
@@ -0,0 +1,82 @@
+//
+//  ScratchView.m
+//  ScratchCard
+//
+//  Created by joshuali on 16/6/28.
+//  Copyright © 2016年 joshuali. All rights reserved.
+//
+
+#import "ScratchView.h"
+
+@interface ScratchView ()
+{
+    CGPoint startPoint;
+}
+@property (nonatomic, strong) CALayer * maskLayer;
+@end
+
+@implementation ScratchView
+
+- (void) awakeFromNib
+{
+    [super awakeFromNib];
+    self.layer.mask = [[CAShapeLayer alloc] initWithLayer:self.layer];
+}
+
+- (void) touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event
+{
+    UITouch *touch = [[event allTouches] anyObject];
+    CGPoint touchLocation = [touch locationInView:self];
+    startPoint = touchLocation;
+}
+
+- (void) touchesMoved:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event
+{
+    UITouch *touch = [[event allTouches] anyObject];
+    CGPoint touchLocation = [touch locationInView:self];
+    CAShapeLayer * layer = [CAShapeLayer new];
+    layer.path = [self getPathFromPointA:startPoint toPointB:touchLocation].CGPath;
+    if(!_maskLayer){
+        _maskLayer = [CALayer new];
+    }
+    [_maskLayer addSublayer:layer];
+    
+    self.layer.mask = _maskLayer;
+    startPoint = touchLocation;
+}
+
+- (void) touchesEnded:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event
+{
+    UITouch *touch = [[event allTouches] anyObject];
+    CGPoint touchLocation = [touch locationInView:self];
+    CAShapeLayer * layer = [CAShapeLayer new];
+    layer.path = [self getPathFromPointA:startPoint toPointB:touchLocation].CGPath;
+    if(!_maskLayer){
+        _maskLayer = [CALayer new];
+    }
+    [_maskLayer addSublayer:layer];
+    
+    self.layer.mask = _maskLayer;
+}
+
+- (UIBezierPath *) getPathFromPointA:(CGPoint)a toPointB : (CGPoint) b
+{
+    UIBezierPath * path = [UIBezierPath new];
+    UIBezierPath * curv1 = [UIBezierPath bezierPathWithArcCenter:a radius:self.scratchLineWidth startAngle:angleBetweenPoints(a, b)+M_PI_2 endAngle:angleBetweenPoints(a, b)+M_PI+M_PI_2 clockwise:b.x >= a.x];
+    [path appendPath:curv1];
+    UIBezierPath * curv2 = [UIBezierPath bezierPathWithArcCenter:b radius:self.scratchLineWidth startAngle:angleBetweenPoints(a, b)-M_PI_2 endAngle:angleBetweenPoints(a, b)+M_PI_2 clockwise:b.x >= a.x];
+    [path addLineToPoint:CGPointMake(b.x * 2 - curv2.currentPoint.x, b.y * 2 - curv2.currentPoint.y)];
+    [path appendPath:curv2];
+    [path addLineToPoint:CGPointMake(a.x * 2 - curv1.currentPoint.x, a.y * 2 - curv1.currentPoint.y)];
+    [path closePath];
+    return path;
+}
+
+CGFloat angleBetweenPoints(CGPoint first, CGPoint second) {
+    CGFloat height = second.y - first.y;
+    CGFloat width = first.x - second.x;
+    CGFloat rads = atan(height/width);
+    return -rads;
+}
+
+@end
